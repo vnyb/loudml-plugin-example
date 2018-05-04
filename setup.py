b2f9125c3eca2e3f6de5190bb631a3de6b10e7bc
Vianney Bajart <vianney.bajart@gmail.com>
2018-05-04 18:38:21 +0200

    add example of plug-in for LoudML

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# This file is part of LoudML HTTP plug-in. LoudML HTTP plug-in is free software:
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Red Mint Network
+
+"""
+Setup plug-in exemple for LoudML
+"""
+
+from setuptools import setup
+
+setup(
+    name='loudml-plugin-example',
+
+    version='1.3.0',
+
+    description="Example of plug-in for LoudML. Fork it to create your own!",
+
+    py_modules = [
+        'loudml_plugin_example',
+    ],
+
+    install_requires=[
+        'loudml',
+        'loudml-api',
+    ],
+
+    entry_points={
+        'loudml.hooks': [
+            'example=loudml_plugin_example:ExampleHook',
+        ],
+    },
+)
